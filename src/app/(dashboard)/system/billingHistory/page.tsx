b34fd1fa3b5296380dff1229a713481f5b53370f
NitishPathania <nitishpathania.dx@gmail.com>
2025-09-22 11:57:45 +0530

    INTEGRATE SIDEBAR

---
+++ src/app/(dashboard)/system/billingHistory/page.tsx
@@ -0,0 +1,13 @@
+// project-imports
+import AgentList from 'views/build/AgentList';
+import KnowledgeBase from 'views/build/KnowledgeBase';
+import SamplePagePage from 'views/other/SamplePage';
+import DemoAgentsViewPage from 'views/build/Demoview';
+import PhoneNumberList from 'views/deploy/PhoneNumbersList';
+import CallHistory from 'views/mointor/CallHistory';
+import BillingHistory from 'views/system/BillingHistory';
+// ==============================|| SAMPLE PAGE ||============================== //
+
+export default function SamplePage() {
+  return <BillingHistory />;
+}
---
+++ src/menu-items/deploy.tsx
@@ -0,0 +1,40 @@
+// assets
+import { Book1,  Security, MessageProgramming, DollarSquare, Airplane } from '@wandersonalwes/iconsax-react';
+import { SupportAgent } from '@mui/icons-material';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+// types
+import { NavItemType } from 'types/menu';
+import SmartToyIcon from '@mui/icons-material/SmartToy';
+import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
+// icons
+const icons = {
+  page: DashboardIcon,
+  authentication: Security,
+  maintenance: MessageProgramming,
+  pricing: DollarSquare,
+  // contactus: I24Support,
+  landing: Airplane,
+  agent:SmartToyIcon
+};
+
+// ==============================|| MENU ITEMS - PAGES ||============================== //
+
+const deploy: NavItemType = {
+  id: 'group-pages',
+  title: 'deploy',
+  type: 'group',
+  icon: icons.page,
+  children: [
+    {
+      id: 'phoneNumbers',
+      title: 'Phone Numbers',
+      type: 'item',
+      url: '/deploy/phoneNumbers',
+      icon: ContactPhoneIcon,
+     
+    },
+ 
+  ]
+};
+
+export default deploy;
